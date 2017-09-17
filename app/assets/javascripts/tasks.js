11565ecc7bd5f5eadf578a724d94e7c0769a73ab
Michael Burnstein <michael@burnstein.com>
2017-09-17 19:02:02 +0000

    App completed

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -5,7 +5,9 @@
     // <li> tags
     function taskHtml(task) {
       var checkedStatus = task.done ? "checked" : "";
-      var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' +
+      var liClass = task.done ? "completed" : "";
+      var liElement = '<li id="listItem-' + task.id +'" class="' + liClass + '">' +
+      '<div class="view"><input class="toggle" type="checkbox"' +
         " data-id='" + task.id + "'" +
         checkedStatus +
         '><label>' +
@@ -31,7 +33,12 @@
         task: {
           done: doneValue
         }
-      });
+      }).success(function(data) {
+        var liHtml = taskHtml(data);
+        var $li = $("#listItem-" + data.id);
+        $li.replaceWith(liHtml);
+        $('.toggle').change(toggleTask);
+      } );
     }
 
 
@@ -48,7 +55,7 @@
       $('.toggle').change(toggleTask);
     });
 
-        $('#new-form').submit(function(event) {
+      $('#new-form').submit(function(event) {
       event.preventDefault();
       var textbox = $('.new-todo');
       var payload = {
